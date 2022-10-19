8321d66a7180d735628a5781bf410e4a2a6aba49
Ksenia Druzhinskaya <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-10-19 12:30:34 +0300

    Ex8

--- src/test/java/Lesson2HomeworkTests.java
+++ src/test/java/Lesson2HomeworkTests.java
@@ -1,12 +1,11 @@
 
 import io.restassured.RestAssured;
-import io.restassured.http.Header;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Lesson2HomeworkTests
 {
@@ -59,6 +58,51 @@ public class Lesson2HomeworkTests
         } while(statusCode != 200);
 
         System.out.println(redirectCount);
+    }
+
+    @Test
+    public void testTokens() throws InterruptedException
+    {
+        String methodPath = "https://playground.learnqa.ru/ajax/api/longtime_job";
+
+        Response response = RestAssured
+                .given()
+                .get(methodPath)
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        if(statusCode != 200){
+            System.out.println("Error. Status code of the first request is " + statusCode);
+            return;
+        }
+        System.out.println("Success. Status code of the first request is " + statusCode);
+
+        JsonPath jsonPath = response.jsonPath();
+        Integer timeout = jsonPath.get("seconds");
+        SECONDS.sleep(timeout);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("token", jsonPath.get("token"));
+
+        jsonPath = RestAssured
+                .given()
+                .queryParams(params)
+                .get(methodPath)
+                .jsonPath();
+
+        String status = jsonPath.get("status");
+        if(!status.equals("Job is ready")){
+            System.out.println("Error. Status is '" + status + "'.");
+        }else{
+            System.out.println("Success. Status is '" + status + "'.");
+        }
+
+        String result = jsonPath.get("result");
+        if(result == null){
+            System.out.println("Error. Result is empty");
+        }else{
+            System.out.println("Success. Result is '" + result + "'.");
+        }
     }
 }
 
