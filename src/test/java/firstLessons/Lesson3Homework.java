bf058b03b7531a363e4978fed9268438a23807e0
Ksenia Druzhinskaya <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-10-21 17:57:36 +0300

    Ex13

--- src/test/java/firstLessons/Lesson3Homework.java
+++ src/test/java/firstLessons/Lesson3Homework.java
@@ -2,16 +2,20 @@ package firstLessons;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static lib.Assertions.assertStringLength;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Lesson3Homework
 {
@@ -55,4 +59,38 @@ public class Lesson3Homework
         });
     }
 
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void testCheckUserAgent(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice){
+
+        JsonPath response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String actualPlatform = response.get("platform");
+        assertEquals(expectedPlatform, actualPlatform, "Incorrect platform for User Agent '" + userAgent + "'");
+        String actualBrowser = response.get("browser");
+        assertEquals(expectedBrowser, actualBrowser, "Incorrect browser for User Agent '" + userAgent + "'");
+        String actualDevice = response.get("device");
+        assertEquals(expectedDevice, actualDevice, "Incorrect device for User Agent '" + userAgent + "'");
+    }
+
+    private static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                        "Mobile", "No", "Android"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+                        "Mobile", "Chrome", "iOS"),
+                Arguments.of("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                        "Googlebot", "Unknown", "Unknown"),
+                Arguments.of("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+                        "Web", "Chrome", "No"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+                        "Mobile", "No", "iPhone")
+        );
+    }
 }
+
+
