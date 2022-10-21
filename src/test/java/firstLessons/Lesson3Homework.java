63c5b0e1f9d6b65bd256d6965ca37985e72db0b2
Ksenia Druzhinskaya <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-10-21 15:31:06 +0300

    Ex11

--- src/test/java/firstLessons/Lesson3Homework.java
+++ src/test/java/firstLessons/Lesson3Homework.java
@@ -1,9 +1,16 @@
 package firstLessons;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Map;
+
 import static lib.Assertions.assertStringLength;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Lesson3Homework
 {
@@ -12,4 +19,23 @@ public class Lesson3Homework
     public void testStringLength(String string){
         assertStringLength(string, 15);
     }
+
+    @Test
+    public void testGetCookie(){
+        Response response = RestAssured
+                .given()
+                .post("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Map<String, String> cookies = response.getCookies();
+        assertTrue(cookies.size() > 0, "Cookie list is empty");
+
+        cookies.forEach((k,v) -> {
+            assertFalse(k.isEmpty(), "Cookie name is empty.");
+            assertFalse(v.isEmpty(), "Cookie value is empty.");
+            System.out.println("Cookie name is '" + k + "', cookie value is '" + v + "'");
+        });
+    }
+
+
 }
