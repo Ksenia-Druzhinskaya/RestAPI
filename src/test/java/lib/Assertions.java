061aa7b6d6adf2acd73781f0f63b2bb43087a99f
Ksenia Druzhinskaya <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-10-21 11:40:50 +0300

    Ex10 fixed

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -9,13 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Assertions
 {
     public static void assertJsonByName(Response response, String name, int expectedValue){
-        //response.then().assertThat().body("$", hasKey(name));
+        response.then().assertThat().body("$", hasKey(name));
 
         int value = response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
     }
 
     public static void assertStringLength(String string, int length){
-        assertTrue(string.length() > 15, "The string is too short to be used");
+        assertTrue(string.length() > length, "The string is too short to be used");
     }
 }
